6cd7e103febf3e6d3f8bc7890989a5db9e1d5bb5
Abhay-raj69 <rajabhay56999@gmail.com>
2024-03-15 12:57:00 +0530

    some basic working principles of nodejs

--- index.js
+++ index.js
@@ -9,44 +9,52 @@
 const fs=require('fs')
 
 const path=require('path')
+const os=require('os')
 
-const dirPath=path.join(__dirname,'file')
 
-//Sync
+console.log(os.cpus().length);
+
+// const dirPath=path.join(__dirname,'file')
+
+// //Sync Blocking request
 // fs.writeFileSync('apple.txt','I like Apples')
 
 
-// for (let i=0;i<5;i++){
-//     fs.writeFileSync(dirPath+"/orange"+i+".txt","a simple fruit but very tasty")
-// }
+// // for (let i=0;i<5;i++){
+// //     fs.writeFileSync(dirPath+"/orange"+i+".txt","a simple fruit but very tasty")
+// // }
 
-// fs.readdir(dirPath,(error,files)=>{
-//     console.warn(files)
-// })
+// // fs.readdir(dirPath,(error,files)=>{
+// //     console.warn(files)
+// // })
 
-// Async
-fs.writeFile("./test.txt","Hello World Async",(error)=>{})
-// here we have used Sync so we can store it in a variable
-const result=fs.readFileSync('./contacts.txt','utf-8')
+// // Async Non- blocking request 
+// fs.writeFile("./test.txt","Hello World Async",(error)=>{})
+// // here we have used Sync so we can store it in a variable
+// const result=fs.readFileSync('./contacts.txt','utf-8')
+// // console.log(result);
+
+
+// // fs.readFile('./contacts.txt','utf-8',(error,result)=>{
+// //     if (error) {
+// //         console.log("Error ",error);
+// //     } else{
+// //         console.log(result);
+// //     }
+// // })
+
+// // used to append any info into existing file
+// fs.appendFileSync("./contacts.txt","\n Rohit 545454")
+
+// // used to copy the data to another file
+// fs.cpSync('./contacts.txt','./copy.txt')
+
+// // used to delete any file
+// fs.unlinkSync('./copy.txt')
+
+// // it will give us all info about the file
+// console.log(fs.statSync('./test.txt'));
+
+
+// const result=fs.readFileSync("test.txt","utf-8")
 // console.log(result);
-
-
-// fs.readFile('./contacts.txt','utf-8',(error,result)=>{
-//     if (error) {
-//         console.log("Error ",error);
-//     } else{
-//         console.log(result);
-//     }
-// })
-
-// used to append any info into existing file
-fs.appendFileSync("./contacts.txt","\n Rohit 545454")
-
-// used to copy the data to another file
-fs.cpSync('./contacts.txt','./copy.txt')
-
-// used to delete any file
-fs.unlinkSync('./copy.txt')
-
-// it will give us all info about the file
-console.log(fs.statSync('./test.txt'));
